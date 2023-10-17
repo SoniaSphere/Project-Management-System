50247fc5fe01fc45d8f437ed1edcc772b17eafa3
Tilak <c0868747@mylambtob.ca>
2023-10-17 04:51:32 -0400

    •member and admin profile page

--- assets/js/member/profile.js
+++ assets/js/member/profile.js
@@ -8,7 +8,6 @@ function displayProfile(){
     var currentUser = JSON.parse(localStorage.getItem("currentUser")); 
 
     var formName = document.getElementById('name');
-
     var formEmail = document.getElementById('email')
 
     const roleDropdown = document.getElementById('role');
@@ -22,6 +21,7 @@ function displayProfile(){
     if(currentUser){
         formName.value = currentUser.name
         formEmail.value = currentUser.email
+
         var roleIndex = (currentUser.role === "member") ? 0 : 1
         roleDropdown[roleIndex].selected = true
     }
@@ -35,9 +35,6 @@ function updateProfile(){
     var name = document.getElementById('name').value;
     var pwd = document.getElementById('password').value;
     var c_pwd = document.getElementById('c_password').value;
-
-    var role = document.getElementById('role')
-    var roleValue = role.options[role.selectedIndex].value
 
     if(name.trim() === "" || pwd.trim() === "" || c_pwd.trim() === ""){
         alert("Please fill in all fields.")
@@ -59,7 +56,7 @@ function updateProfile(){
         userID: currentUser.userID,
         email: currentUser.email,
         password: pwd,
-        role: roleValue,
+        role: currentUser.role,
         name: name
     }
     users[users.findIndex(user => user.userID == currentUser.userID)] = updatedUser
@@ -70,20 +67,25 @@ function updateProfile(){
     // Save the updated users back to localStorage
     localStorage.setItem("users", JSON.stringify(users));
 
-    //if new role is a member
-    if(roleValue === "member"){
-        // Remove the current user information from local storage
-        localStorage.removeItem('currentUser');
-
-        // Redirect to the login page (you may adjust the URL as needed)
-        window.location.href = "../../index.html";
-        return
-    }
-    
     alert("Profile Updated Successfully.")
+    displayProfile()
 
-    
-    
 }
 
+// const profilePicture = document.getElementById('profile_pic');
+// const profilePictureInput = document.getElementById('profile-picture-input');
+
+// profilePicture.addEventListener('click', () => {
+//     profilePictureInput.click();
+// });
+
+// profilePictureInput.addEventListener('change', () => {
+//     const file = profilePictureInput.files[0];
+//     if (file) {
+//         const url = URL.createObjectURL(file);
+//         profilePicture.src = url;
+//     }
+// });
+
+// Initialize project list on page load
 window.onload = displayProfile;
